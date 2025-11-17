7d45df30edb40bffdaf21f8410c41a1d3cbef2d6
Thierry-Bryan <bryan.thierry@edu.univ-fcomte.fr>
2025-11-17 21:03:37 +0100

    fix: Finalisation du problème de chemin d'accès statique (ASSETS_DIR et base URL)

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://portfolio.bryan-thierry.fr",
   // NOUVEAU : Configure Astro pour créer un build SSR (Server-Side Rendering)
   output: "server",
   // NOUVEAU : Utilise l'adaptateur Node pour générer le code de serveur
